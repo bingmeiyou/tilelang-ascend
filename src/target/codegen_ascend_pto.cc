25640482cf653c984e516504aeac91d8af22b61a
bingmeiyou <zhoushaofan@huawei.com>
2026-07-06 16:17:42 +0800

    style: apply clang-format to codegen_ascend_pto.cc
    Format long lines to comply with clang-format style requirements:
    - Break alignment calculation across multiple lines
    - Break TASSIGN stream output across multiple lines

--- src/target/codegen_ascend_pto.cc
+++ src/target/codegen_ascend_pto.cc
@@ -2025,7 +2025,8 @@ void CodeGenTileLangAscendPto::TransposeCodegen(const CallNode *op,
   int32_t tmp_buffer_size = N * tmp_tile_w * elem_bytes;
   max_ub_addr_ += tmp_buffer_size;
   // Align to 32-byte boundary
-  max_ub_addr_ = ((max_ub_addr_ + kUbAlignmentBytes - 1) / kUbAlignmentBytes) * kUbAlignmentBytes;
+  max_ub_addr_ = ((max_ub_addr_ + kUbAlignmentBytes - 1) / kUbAlignmentBytes) *
+                 kUbAlignmentBytes;
 
   this->PrintIndent();
   this->stream << "{\n";
@@ -2638,13 +2639,15 @@ void CodeGenTileLangAscendPto::SiluCodegen(const CallNode *op) {
   int32_t tmp_buffer_size = row * col * elem_bytes;
   max_ub_addr_ += tmp_buffer_size;
   // Align to 32-byte boundary
-  max_ub_addr_ = ((max_ub_addr_ + kUbAlignmentBytes - 1) / kUbAlignmentBytes) * kUbAlignmentBytes;
+  max_ub_addr_ = ((max_ub_addr_ + kUbAlignmentBytes - 1) / kUbAlignmentBytes) *
+                 kUbAlignmentBytes;
 
   this->PrintIndent();
   this->stream << "tl::ascend_pto::TileUbDataND<" << dst_shape_info.type << ", "
                << row << ", " << col << "> " << tmp_name << ";\n";
   this->PrintIndent();
-  this->stream << "TASSIGN(" << tmp_name << ", " << max_ub_addr_ - tmp_buffer_size << ");\n";
+  this->stream << "TASSIGN(" << tmp_name << ", "
+               << max_ub_addr_ - tmp_buffer_size << ");\n";
   this->PrintIndent();
   this->stream << kAscendPtoScope << "TSILU<" << dst_shape_info.type << ", "
                << row << ", " << col << ">(" << dst_name << ", " << src_name
@@ -2672,13 +2675,15 @@ void CodeGenTileLangAscendPto::MulAddDstCodegen(const CallNode *op) {
   int32_t tmp_buffer_size = row * col * elem_bytes;
   max_ub_addr_ += tmp_buffer_size;
   // Align to 32-byte boundary
-  max_ub_addr_ = ((max_ub_addr_ + kUbAlignmentBytes - 1) / kUbAlignmentBytes) * kUbAlignmentBytes;
+  max_ub_addr_ = ((max_ub_addr_ + kUbAlignmentBytes - 1) / kUbAlignmentBytes) *
+                 kUbAlignmentBytes;
 
   this->PrintIndent();
   this->stream << "tl::ascend_pto::TileUbDataND<" << dst_shape_info.type << ", "
                << row << ", " << col << "> " << tmp_name << ";\n";
   this->PrintIndent();
-  this->stream << "TASSIGN(" << tmp_name << ", " << max_ub_addr_ - tmp_buffer_size << ");\n";
+  this->stream << "TASSIGN(" << tmp_name << ", "
+               << max_ub_addr_ - tmp_buffer_size << ");\n";
   this->PrintIndent();
   this->stream << kAscendPtoScope << "MulAddDst<" << dst_shape_info.type << ", "
                << row << ", " << col << ">(" << dst_name << ", " << src0_name
